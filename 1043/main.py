49b146c24ee4e649ce54c70d5bb7e032aa18d4ab
andsantos <andsouzasanttos@gmail.com>
2022-04-24 12:01:19 -0300

    Continuação lista 2 Ubuntu-PC

--- 1043/main.py
+++ 1043/main.py
@@ -1,4 +1,13 @@
 
 A, B, C = map(float, input().split())
 
-abs(B - C) < A < B + C
+condition1 = abs(B - C) < A < B + C
+condition2 = abs(A - C) < B < A + C
+condition3 = abs(A - B) < C < A + B
+
+if condition1 and condition2 and condition3:
+    perimetro = A + B + C
+    print(f'Perimetro = {perimetro:.1f}')
+else:
+    area =  (A + B) * C / 2
+    print(f'Area = {area:.1f}')
